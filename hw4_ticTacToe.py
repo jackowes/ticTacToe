8951e67d11735f0cf3f464542da735b4fc625130
Ryan Huynh <ryanhuynh@mu-586534.dhcp.missouri.edu>
2022-10-21 10:18:50 -0500

    more changes to code and eval function

--- hw4_ticTacToe.py
+++ hw4_ticTacToe.py
@@ -24,9 +24,9 @@ def min_max(curr_game, max_or_min, depth, max_depth):
         #Check for win
         if curr_game.win_check() == True:
             if max_or_min == MAXIMIZING: #this needs to be changed to max or min I think
-                return 1000
+                return -1000
             elif max_or_min == MINIMIZING:
-                return -1000       
+                return 1000       
 
         #Check for draw
         elif curr_game.win_check() == False and len(available_moves) == 0:
@@ -34,7 +34,7 @@ def min_max(curr_game, max_or_min, depth, max_depth):
 
         #Check when max depth is hit
         else:
-            eval = eval_board(curr_game.board, curr_game.get_x_or_o)
+            eval = eval_board(curr_game, curr_game.current_player())
             return eval
  
 
@@ -48,12 +48,12 @@ def min_max(curr_game, max_or_min, depth, max_depth):
         
         # Searches for best possible maxmizing move
         for child in available_moves:
-            row, col = child.split(",") #does it modify?
+            row, col = child #does it modify?
 
             new_game = copy.deepcopy(curr_game)
 
             
-            new_game.board[int(row) - 1][int(col) - 1] = new_game.get_x_or_o()
+            new_game.board[int(row) - 1][int(col) - 1] = new_game.current_player()
             #new_game.available.remove("{0},{1}".format(row, col))
             new_game.change_players()
             
@@ -66,7 +66,9 @@ def min_max(curr_game, max_or_min, depth, max_depth):
                 max_move = child
             
         # Return move if we finish searching or max eval if we still need to search
-        if depth == max_depth: return max_move
+        if depth == 0: 
+            print("max move score:", max_eval)
+            return max_move
         return max_eval
             
     
@@ -77,22 +79,24 @@ def min_max(curr_game, max_or_min, depth, max_depth):
         min_eval = 1000
         min_move = available_moves[0]
         for child in available_moves:
-            row, col = child.split(",") #does it modify?
+            row, col = child
 
             new_game = copy.deepcopy(curr_game)
 
             
-            new_game.board[int(row) - 1][int(col) - 1] = new_game.get_x_or_o()
+            new_game.board[int(row) - 1][int(col) - 1] = new_game.current_player()
             # new_game.available.remove("{0},{1}".format(row, col))
             new_game.change_players()
 
-            eval = min_max(new_game, MAXIMIZING, depth + 1)
+            eval = min_max(new_game, MAXIMIZING, depth + 1, max_depth)
 
             if eval < min_eval: 
                 min_eval = eval
                 min_move = child
 
-        if depth == max_depth: return min_move
+        if depth == 0: 
+            print("min move score:", min_eval)
+            return min_move
         return min_eval
 
 
@@ -139,7 +143,7 @@ def eval_row(player_char, row):
     the_check()
     return (threeWtwo, threeWone, twoWtwo, twoWone)
 
-def eval_board(board, player_char):
+def eval_board(ticGame, player_char):
     #Eval Each board
     score = 0
     # board =            [["-","O","O","O","-","-"],
@@ -148,7 +152,9 @@ def eval_board(board, player_char):
     #                     ["X","X","-","-","-","-"], 
     #                     ["-","-","-","O","O","O"]]
     # player_char = "X"
-    
+    board = ticGame.board
+
+
     opponent_char = ""
     #Set player and opponent
     if player_char == "X":
@@ -156,23 +162,30 @@ def eval_board(board, player_char):
     else:
         opponent_char = "X"
 
+
+    if ticGame.win_check_side(player_char):
+        return 1000
+    elif ticGame.win_check_side(opponent_char):
+        return -1000
+
     for row in board:
         #Eval current player
         (threeWtwo, threeWone, twoWtwo, twoWone) = eval_row(player_char, row)
         score += (200 * threeWtwo) + (150 * threeWone) + (20 * twoWtwo) + (5 * twoWone)
-        print("Player Score + " + str((200 * threeWtwo) + (150 * threeWone) + (20 * twoWtwo) + (5 * twoWone)))
+        #print("Player Score + " + str((200 * threeWtwo) + (150 * threeWone) + (20 * twoWtwo) + (5 * twoWone)))
         
         #Eval opponent player
         (threeWtwo, threeWone, twoWtwo, twoWone) = eval_row(opponent_char, row)
         score -= (80 * threeWtwo) + (40 * threeWone) + (15 * twoWtwo) + (2 * twoWone)
-        print("Opponent Score + " + str((80 * threeWtwo) + (40 * threeWone) + (15 * twoWtwo) + (2 * twoWone)) + "\n")
+        #print("Opponent Score + " + str((80 * threeWtwo) + (40 * threeWone) + (15 * twoWtwo) + (2 * twoWone)) + "\n")
     
     # print(threeWone)
     # print(threeWtwo)
     # print(twoWone)
     # print(twoWtwo)
     # print("Total score= ", score)
-   
+    return score
+
 #The psuedocode
 # 2with 1 = 0
 # 2 with 2 = 0
@@ -205,9 +218,7 @@ class ticTacToe:
         Initialize new ticTacToe game
         """
         self.player = X
-        self.has_won = False #Not sure if this is needed I think just a win check function could be better
         self.board = [] 
-        self.available = [] #not sure if this is needed anymore
         self.moves = [] #This will be an array of 4 valued tuples: (Turn, The move, CPU Execution time, # of nodes searched)
     #=======================================
     def create_board(self):
@@ -228,7 +239,16 @@ class ticTacToe:
                         ["-","-","-","-","-","-"], 
                         ["-","-","-","-","-","-"]]
 
-
+        # self.board =   [["-","-","-","-","-","-"],
+        #                 ["-","-","-","-","-","-"],
+        #                 ["-","-","-","-","-","-"], 
+        #                 ["-","-","-","-","-","-"], 
+        #                 ["0","-","-","-","-","-"]]
+        
+        # self.board[random.randrange(0,5)][random.randrange(0,6)] = "X"
+        # self.board[random.randrange(0,5)][random.randrange(0,6)] = "O"
+        
+        
         #As the available squares are now only adjacent squares I don't think this is usable any longer
         # for i in range(1, 5):
         #     for j in range(1, 7):
@@ -246,6 +266,8 @@ class ticTacToe:
                 print(y, end=" ")
             print("")
 
+        print("\n")
+
     #=======================================
     def change_players(self):
         """
@@ -257,7 +279,7 @@ class ticTacToe:
             self.player = X
 
     #=======================================
-    def get_x_or_o(self):
+    def current_player(self):
         """
         Obtains whose current turn it is
 
@@ -310,13 +332,22 @@ class ticTacToe:
 
     #=======================================
     def win_check(self):
+        current_player = player = self.current_player() #the currentPlayer? Or will this need to be the last played #lets just make it universal
+        opponent_player = ""
+
+        if current_player == "X":
+            opponent_player = "O"
+        else:
+            opponent_player = "X"
+        
+        return self.win_check_side(current_player) or self.win_check_side(opponent_player)
+
+    def win_check_side(self, player):
         """
         Determines whether or not any player has won.
         """
         #I need to check every row and every column then check the 8 diagonals
-        player = self.get_x_or_o() #the currentPlayer? Or will this need to be the last played #lets just make it universal
-
-
+        
         #check rows
         for row in range(0, 5):
             count = 0
@@ -366,7 +397,7 @@ class ticTacToe:
         for start in tr_diagonals:
             spot = list(start)
             count = 0
-            print(spot)
+            # print(spot)
             while spot[0] < 5 and spot[1] >= 0:
                 
                 if(self.board[spot[0]][spot[1]] == player):
@@ -405,9 +436,29 @@ if __name__ == '__main__':
     ticGame = ticTacToe()
     ticGame.create_board()
 #    moves = ticGame.successor_function()
-
-    while 
-
+    depth = 2
     ticGame.display()
-    print(f"\n{moves}")
-    print(f"\n{ticGame.win_check()}")
+    while ticGame.win_check() == False and len(ticGame.successor_function()) != 0:
+        if ticGame.current_player() == "X":
+            move = min_max(ticGame, MAXIMIZING, 0, 3)
+            print("X move:", move)
+        else:
+            move = min_max(ticGame, MAXIMIZING, 0, 3)
+            print("O move:", move)
+            
+
+        ticGame.board[move[0]][move[1]] = ticGame.current_player()
+        ticGame.change_players()
+
+        ticGame.display()
+        #time.sleep(2)
+    
+    if len(ticGame.successor_function()) == 0:
+        print("DRAW")
+    else:
+        if ticGame.win_check_side("X"):
+            print("X won")
+        else:
+            print("O won")
+        
+
